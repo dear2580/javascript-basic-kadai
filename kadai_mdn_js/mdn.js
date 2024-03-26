4fa97fbbfad73d29884e70ac52ad6ba727d92198
dear2580 <157022467+dear2580@users.noreply.github.com>
2024-03-26 21:56:30 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date('August 22, 1975');
+
+const date = () => {
+    console.log(today.getFullYear());
+    console.log(today.getMonth()+ 1);
+    console.log(today.getDay());
+}
+
+date();
