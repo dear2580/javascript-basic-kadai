07bd49648fcd962acdd83e7580decb28c89b3f5b
dear2580 <157022467+dear2580@users.noreply.github.com>
2024-04-07 15:59:44 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-const parentList = document.getElementById('parent-list');
-btn.addEventListener('click', () => {
-    const childList = document.createElement('h1');
-    childList.textContent = 'ボタンをクリックしました';
-    parentList.appendChild(childList);
-})
-
